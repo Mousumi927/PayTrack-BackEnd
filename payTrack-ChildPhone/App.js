1c3496c81958a4ee50fe79b829947d93fea7d312
Jihoon (JJ) Jung <Nnoitra5+GitHub@gmail.com>
2023-06-21 15:06:56 -0600

    Minor changes for now.

--- payTrack-ChildPhone/App.js
+++ payTrack-ChildPhone/App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./home.jsx";
 import LoginScreen from "./login.jsx";
+import RequestMoneyScreen from "./ask_money.jsx";
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
       <HomeScreen />
       <LoginScreen />
+      <RequestMoneyScreen />
     </View>
   );
 };
