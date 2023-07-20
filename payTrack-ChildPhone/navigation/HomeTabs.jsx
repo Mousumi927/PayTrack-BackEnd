90da65c9d70012922a6be6aa5ac7a1e7a7489f71
Mousumi Saha <mousumisaha927@gmail.com>
2023-07-19 18:51:33 -0600

    minor modification in Home page

--- payTrack-ChildPhone/navigation/HomeTabs.jsx
+++ payTrack-ChildPhone/navigation/HomeTabs.jsx
@@ -3,8 +3,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import Request from "../pages/Request";
 import History from "../pages/History";
-import Pay from "../pages/Pay";
-import Home from "../pages/Home";
+import Pay from "../pages/pay";
+import Home from "../pages/home";
 
 const HomeTabs = () => {
   const Stack = createNativeStackNavigator();
@@ -12,7 +12,7 @@ const HomeTabs = () => {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
 
-      <Stack.Screen name="Request" component={Request} />
+      <Stack.Screen name="Request Money" component={Request} />
       <Stack.Screen name="Pay" component={Pay} />
       <Stack.Screen name="History" component={History} />
     </Stack.Navigator>
